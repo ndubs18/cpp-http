09b5229d0107c60dd6c17a496a434e042e9aa600
ndubs18 <nick.winkler1@gmail.com>
2025-12-07 08:13:11 -0800

    parse and display by line

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-#include<fstream>
-#include<iostream>
+#include <cstddef>
+#include <cstring>
+#include <fstream>
+#include <iostream>
 
 const std::string FILE_PATH = "./messages.txt"; 
-	const size_t CHUNK_WIDTH = 8;
+const size_t CHUNK_WIDTH = 8;
+const int CHUNK_WIDTH_INT = static_cast<int>(CHUNK_WIDTH);
+
+int hasNewline(const char* str);
+void parseNewlineChunk(const char* chunk, std::string& line, size_t len);
 
 int main(int argc, char** argv) {
 	std::ifstream stream {std::ifstream(FILE_PATH)};
@@ -18,21 +24,50 @@ int main(int argc, char** argv) {
 		std::cout << "No text in the file\n";
 		return 0;
 	};
-
+	
+	std::string message = {};
 	char chunk[CHUNK_WIDTH + 1] {};
 
 	while (stream.read(chunk, CHUNK_WIDTH)) {
-		std::cout << stream.gcount() << " ";
 		chunk[CHUNK_WIDTH] = '\0';
-		std::cout << "read: " << chunk << "\n";
+		parseNewlineChunk(chunk, message, CHUNK_WIDTH)	;
 	}
 
-	if (stream.gcount()) {	
-		chunk[stream.gcount()] = '\0';
-		std::cout << "read: " << chunk << "\n";
+	int gc = stream.gcount();
 
+	if (gc) {	
+		chunk[gc] = '\0';
+		parseNewlineChunk(chunk, message, gc);
+	} else {
+		message.append(chunk, gc);
 	}
-
+	
 	stream.close();
 	return 0;
 }
+
+void parseNewlineChunk(const char* chunk, std::string& message, size_t len) {
+	int nl = hasNewline(chunk);
+
+	if (nl>=0) {
+		message.append(chunk, nl+1);	
+		std::cout << message;
+		message.clear();
+		
+		if (nl+1 < static_cast<int>(len)) {
+			message.append(chunk+nl+1, len-(nl+1));
+		}
+	} else {
+		message.append(chunk, len);
+	}
+}
+
+int hasNewline(const char* str) {
+	for(int i=0; i<CHUNK_WIDTH_INT; ++i) {
+		if (str[i] == '\n') {
+			return i;
+		}
+	}
+	return -1;
+}
+
